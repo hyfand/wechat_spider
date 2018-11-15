0a2a3d684a9ca1e1264e9616b25eef65bebf9715
manitohan <303498033@qq.com>
2018-11-15 09:02:11 +0800

    Update upload_data.py

--- auto_driver/upload_data.py
+++ auto_driver/upload_data.py
@@ -1,112 +1,192 @@
-from time import sleep
-from redis import *
-from appium import webdriver
-import os
+from pymongo import *
+from config import *
+import datetime, time
+import requests
+import json
+from elasticsearch import Elasticsearch
+import copy
 
-desire_caps = {}
-desire_caps['platformName'] = 'Android'
-# 诺基亚6
-# desire_caps['platformVersion'] = '8.1.0'
-# desire_caps['deviceName'] = 'PL2GAM1810904175'
+mongo_client = MongoClient(host=MONGO['host'], port=MONGO['port'])
+wechat_db = mongo_client.get_database(WECHAT_DB_NAME)
 
-# 小米6
-# desire_caps['platformVersion'] = '8.0.0'
-# desire_caps['deviceName'] = '1231acb'
+posts_col = wechat_db.get_collection(POST_COL)  # 文章列表集合
+profile_col = wechat_db.get_collection(PROFILE_COL)  # 公众号集合
+comment_col = wechat_db.get_collection(COMMENTS_COL)  # 评论集合
 
-# 红米5
-desire_caps['platformVersion'] = '7.1.2'
-desire_caps['deviceName'] = '2466979e7cf5'
+# es客户端
+es = Elasticsearch(
+    hosts=[{'host': ES['host'], 'port': ES['port']}],
+    http_auth=(ES['auth'], ES['password'])
+)
 
-desire_caps['appPackage'] = 'com.tencent.mm'
-desire_caps['appActivity'] = '.ui.LauncherUI'
-# 以下两项主要是在点击输入框的时候,会触发系统输入法,导致可能我们发送的是字符 `234`,但是九宫格中文输入法有可能给出的是 `bei` ,这两个属性就是屏蔽系统输入法,使用appium自己的,但是测试完成后,得自己去系统设置中将输入法切换过来
-desire_caps['unicodeKeyboard'] = True
-desire_caps['resetKeyboard'] = True
-# 不重置apk
-desire_caps['noReset'] = True
-desire_caps["newCommandTimeout"] = 172800  # 等待下一条命令延时 2 天
-# desire_caps['chromeOptions'] = {'androidProcess': 'com.tencent.mm:tools'}
+# db.getCollection('posts').find({'updateNumAt':{$gte:ISODate('2018-06-11 T00:00:00.00Z')}})
+# 2018-06-12 09:56:39.989755
+# 2017-04-11 T00:00:00.00Z
 
-# ip地址在pc上的 appium客户端-设置 中可以看到 `server address` 和 `port`,保持一致即可
-driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desire_caps)
+'''
+    先判断远端数据量是否为0，如果是0则是第一次上传，那么上传全部。
+    否则只上传当天
+'''
+count_dict = requests.get('%s/if_allupload_wechat' % REMOTE_HOST)
 
-# 先进行公众号有效性检测， 已经迁移的无用公众号从mongo里删除
-current_file_path = os.path.dirname(__file__)
-print('[NOTICE]' + ' Clear invalid wechat profiles')
-script_name = 'check_profiles.py'
-script_path = os.path.join(current_file_path, script_name).replace('\\', '/')
-os.system('python %s' % script_path)
+counts = json.loads(count_dict.content.decode('utf-8'))
 
-sr = StrictRedis()
-sr.delete('post_finish')  # 删掉redis 文章列表爬完标记
-sr.delete('profile_finish') # 删掉redis 公众号走完一遍标记
+print(counts)
 
-sleep(10) # 等加载出通讯录。。。
+if counts['post_count'] == 0 or counts['comment_count'] == 0 or counts['profile_count'] == 0:
+    '''上传全部'''
+    post_res = posts_col.find()
+    comment_res = comment_col.find()
+    profile_res = profile_col.find()
 
-# 1.点击通讯录
-el_contact = driver.find_elements_by_android_uiautomator("new UiSelector().text(\"通讯录\")")[0]
-el_contact.click()
-
-# 2.点击公众号
-el_public = driver.find_elements_by_android_uiautomator("new UiSelector().text(\"公众号\")")[0]
-el_public.click()
-
-# 3.第一个公众号
-# driver.tap([[210, 334], [306, 399]], 20)
-# el_public = driver.find_elements_by_android_uiautomator("new UiSelector().text(\"差评\")")[0]
-el_public = driver.find_elements_by_id("com.tencent.mm:id/zy")[0].find_elements_by_class_name("android.widget.LinearLayout")[0].find_element_by_class_name("android.widget.LinearLayout")
-el_public.click()
-
-# 4.查看历史消息
-el_info = driver.find_element_by_accessibility_id("聊天信息")
-el_info.click()
+else:
+    utc_now = datetime.datetime.now() - datetime.timedelta(days=10)
+    # utc_now = datetime.datetime.now()
+    post_res = posts_col.find({'publishAt': {'$gte': datetime.datetime(utc_now.year, utc_now.month, utc_now.day)}})
+    comment_res = comment_col.find({'updatedAt': {'$gte': datetime.datetime(utc_now.year, utc_now.month, utc_now.day)}})
+    # profile_list 基本每天都会更新 updatedAt 的日期，所以本质跟上面 profile_col.find() 一样
+    profile_res = profile_col.find({'updatedAt': {'$gte': datetime.datetime(utc_now.year, utc_now.month, utc_now.day)}})
 
 
-sleep(3)
-# driver.swipe(530, 1900, 530, 200, 200) # 滑动到底部  1080p
-driver.swipe(350, 1260, 360, 480, 200) # 滑动到底部  720p 红米5
+# I.先把公众号[ id 对应 title ]得map dict搞出来
+id_profile_map = {}
+profiles = profile_col.find()  # 防止重置冲突不能用profile_res
+for item in profiles:
+    id_profile_map[item['msgBiz']] = item['title']
 
-history = driver.find_elements_by_android_uiautomator("new UiSelector().text(\"全部消息\")")[0]
-history.click()
+# II.每条数据 传入Elastic服务器数据
+posts_list = posts_col.find()
+for i in copy.deepcopy(post_res):  # 第一次传全部posts_list，以后则更改为post_res
 
-sleep(10)  # 设置100秒等待，100秒后再循环问数据库是否爬完公众号列表
+    if 'content' in i and i['title'] is not None:
+        doc = dict(
+            title=i['title'],
+            topic=id_profile_map.get(i['msgBiz'], None),
+            abstract=i['digest'],
+            content=i['content'].replace('\xa0', ''),
+            crawl_date=(i['createdAt'] + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S'),
+            news_date=(i['publishAt'] + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S'),
+            news_url=i['link'],
+            referer_web=id_profile_map.get(i['msgBiz'], None),
+            tags=id_profile_map.get(i['msgBiz'], None),
+            order_date=int((time.mktime(i['publishAt'].timetuple()) + 14400 * 2) / 86400)
+        )
 
-# ------------------------------------------
-# a.循环询问redis是否爬完公众号列表 redis_key:'profile_finish'
+        # print(doc)
+        # try:
+        #     es.index(index=ES['index'], doc_type=ES['doc_type'], body=doc)
+        #     print('成功传入es')
+        # except Exception as e:
+        #     print('失败传入es, 原因是:', e)
 
-while True:
-    sleep(1)  # 1秒查询一次redis
-    result = sr.get('profile_finish')
-    if result == b'1':
-        print('[NOTICE]' + ' Appium will click detail page')
-        sr.delete('profile_finish')  # 删掉redis 公众号爬完记录 【修改到node代码一开始启动删除】
-        break
+        # 先查询是否存在，再插入es
+        try:
+            # 查询是否存在
+            sql_body = {
+                "query": {
+                    "multi_match": {
+                        "query": i['link'],
+                        "type": "phrase",
+                        "slop": 0,
+                        "fields": [
+                            "news_url"
+                        ],
+                    }
+                }
+            }
 
-# 5.点击一个详情页触发详情页
-sleep(50)
-# driver.tap([(530, 1300)], 200)  # 点击第一篇文章的位置进入详情 1080p
-print('[NOTICE]' + 'click a detail')
-driver.tap([(360, 840)], 200)  # 点击第一篇文章的位置进入详情 红米
+            res1 = es.search(index=ES['index'], doc_type=ES['doc_type'], body=sql_body)
+            res2 = es.search(index=ES['index_total'], doc_type=ES['doc_type'], body=sql_body)
 
-# b.再次循环询问redis是否爬玩所有文章 redis_key:'post_finish'
-while True:
-    sleep(1)  # 1秒查询一次redis
-    result = sr.get('post_finish')
-    if result == b'1':
-        print('[NOTICE]' + ' Finish crawl')
-        sr.delete('post_finish')  # 删掉redis 文章列表爬完记录
-        break
 
-sleep(10)  # 给个缓冲时间
-print('[NOTICE]' + ' Appium python script quit')
-driver.quit()
+            if int(res1['hits']['total']) == 0:
+                print('+++++新内容插入elastic %s: %s' % (ES['index'], i['title']))
+                es.index(index=ES['index'], doc_type=ES['doc_type'], body=doc)
+            else:
+                print('xxxxx此内容已存在索引 %s: %s, 数量:%s' % (ES['index'], i['title'], res1['hits']['total']))
 
-print('[NOTICE]' + ' Upload Wechat info to Remote Server')
+            if int(res2['hits']['total']) == 0:
+                print('+++++新内容插入elastic: %s: %s' % (ES['index_total'], i['title']))
+                es.index(index=ES['index_total'], doc_type=ES['doc_type'], body=doc)
+            else:
+                print('xxxxx此内容已存在索引 %s: %s, 数量:%s' % (ES['index_total'], i['title'], res2['hits']['total']))
 
-script_name = 'upload_data.py'
-script_path = os.path.join(current_file_path, script_name).replace('\\', '/')
-os.system('python %s' % script_path)
-print('[NOTICE]' + ' Upload Compelete')
+        except Exception as e:
+            print(e, 'elastic exception...')
 
-print('[NOTICE]' + ' 60s later close')
-sleep(60)
+# III.刷新 elasticsearch index
+es.indices.refresh(index=ES['index'])
+es.indices.refresh(index=ES['index_total'])
+
+# a. 文章列表
+post_list = []
+
+
+
+for item in copy.deepcopy(post_res):
+
+    #  没有content应该是和配置时间不符没有爬取，
+    if 'content' in item and item['title'] is not None:
+        item['_id'] = str(item['_id'])
+
+        # 把时间变为本地时间
+        # print(item['_id'], item['title'], item['createdAt'], item['publishAt'])
+        item['createdAt'] = str(item['createdAt'] + datetime.timedelta(hours=8))
+        item['publishAt'] = str(item['publishAt'] + datetime.timedelta(hours=8))
+        item['updatedAt'] = str(item['updatedAt'] + datetime.timedelta(hours=8))
+        try:
+            item['updateNumAt'] = str(item['updateNumAt'] + datetime.timedelta(hours=8))
+        except Exception as e:
+            print(e)
+        post_list.append(item)
+
+    else:
+        print(item['title'], ' | 没有爬到正文')
+# b. 评论列表
+comment_list = []
+for item in comment_res:
+
+    item['_id'] = str(item['_id'])
+    item['postId'] = str(item['postId'])
+    # 每个评论的回复id也要str化
+    if len(item['replies']) != 0:
+        for i in item['replies']:
+            i['_id'] = str(i['_id'])
+            i['createTime'] = str(i['createTime'] + datetime.timedelta(hours=8))
+
+    # 把时间变为本地时间
+    item['createTime'] = str(item['createTime'] + datetime.timedelta(hours=8))
+    item['createdAt'] = str(item['createdAt'] + datetime.timedelta(hours=8))
+    item['updatedAt'] = str(item['updatedAt'] + datetime.timedelta(hours=8))
+    comment_list.append(item)
+# c 公众号列表
+profile_list = []
+for item in profile_res:
+    item['_id'] = str(item['_id'])
+
+
+    # 把时间变为本地时间
+    item['createdAt'] = str(item['createdAt'] + datetime.timedelta(hours=8))
+    item['openHistoryPageAt'] = str(item['openHistoryPageAt'] + datetime.timedelta(hours=8))
+    item['updatedAt'] = str(item['updatedAt'] + datetime.timedelta(hours=8))
+    profile_list.append(item)
+
+# 加个密码验证， 防止接口泄露， 被恶意注入数据
+secret_key = '234sf3gsbx443gsgsrts34gsd43tsd'
+
+final_dict = dict(
+    secret_key=secret_key,
+    post_list=post_list,
+    comment_list=comment_list,
+    profile_list=profile_list
+)
+
+print('公众号数量:{}\r\n文章数量:{}\r\n评论数量:{}\r\n'.format(len(profile_list), len(post_list), len(comment_list)))
+
+json_data = json.dumps(final_dict, ensure_ascii=False)
+
+# 向服务器api发送新爬取的数据
+headers = {'Content-Type': 'application/json'}
+requests.post('%s/receive_wechat' % REMOTE_HOST, headers=headers, data=json_data.encode('utf-8'))
+
+print('已向远程主机发出数据')
